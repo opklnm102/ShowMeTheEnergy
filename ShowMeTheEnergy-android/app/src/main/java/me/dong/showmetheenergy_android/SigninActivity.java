2f5825981095e025535dadc31f07958264a29ff7
hack0325 <fupiu@naver.com>
2016-03-25 19:45:20 +0900

    add SignActivity

--- ShowMeTheEnergy-android/app/src/main/java/me/dong/showmetheenergy_android/SigninActivity.java
+++ ShowMeTheEnergy-android/app/src/main/java/me/dong/showmetheenergy_android/SigninActivity.java
@@ -3,16 +3,11 @@ package me.dong.showmetheenergy_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SigninActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        
+        setContentView(R.layout.activity_signin);
     }
-
-
 }
